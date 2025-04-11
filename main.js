cb50155ef589d453988e92bb8c109ea360068f83
luljn <mbohlulajonathan4@gmail.com>
2025-04-11 23:12:53 +0200

    Default size of the window changed to scale the screen size by default

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 const { app, BrowserWindow } = require('electron/main')
 
 const createWindow = () => {
+    const { screen } = require('electron')
+    const primaryDisplay = screen.getPrimaryDisplay()
+    const { width, height } = primaryDisplay.workAreaSize
+
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width,
+        height,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
     })
     win.loadFile('sign-in.html')
 }
 
 app.whenReady().then(() => {
     createWindow()
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
 })
