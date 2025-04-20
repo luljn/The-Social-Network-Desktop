8586237876956e82c08ee44a9b059dd9fc264a4c
luljn <mbohlulajonathan4@gmail.com>
2025-04-20 20:15:57 +0200

    Navbar updated on about page

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const createWindow = () => {
             contextIsolation: false,
         }
     })
-    win.loadFile('src/home.html')
+    win.loadFile('src/sign-in.html')
 }
 
 app.whenReady().then(() => {
